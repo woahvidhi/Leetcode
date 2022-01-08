d4972ee28ef3f9e9b1884f1417988b497490abe8
Vidhi Shah <61038939+woahvidhi@users.noreply.github.com>
2022-01-08 20:35:10 +0530

    Create SearchInsert.java

---
+++ Algorithm/Java/SearchInsert.java
@@ -0,0 +1,47 @@
+
+// Source : https://leetcode.com/problems/search-insert-position/
+// Author : Vidhi Shah
+// Date   : 09-01-2022
+
+/*
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+*/
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        int mid = (start+end)/2;
+        while(start<=end){
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) end = mid-1;
+            else start=mid+1;
+            mid=(start+end)/2;
+        }
+        return start;
+    }
+}
