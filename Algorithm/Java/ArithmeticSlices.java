62308bcd4ed07b14413cb9e2b991fb6466b07e87
Vidhi Shah <61038939+woahvidhi@users.noreply.github.com>
2022-03-03 23:48:40 +0530

    Create ArithmeticSlices.java

---
+++ Algorithm/Java/ArithmeticSlices.java
@@ -0,0 +1,44 @@
+// Source : https://leetcode.com/problems/arithmetic-slices/
+// Author : Vidhi Shah
+// Date   : 03-03-2022
+/*
+An integer array is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+
+For example, [1,3,5,7,9], [7,7,7,7], and [3,-1,-5,-9] are arithmetic sequences.
+Given an integer array nums, return the number of arithmetic subarrays of nums.
+
+A subarray is a contiguous subsequence of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: 3
+Explanation: We have 3 arithmetic slices in nums: [1, 2, 3], [2, 3, 4] and [1,2,3,4] itself.
+Example 2:
+
+Input: nums = [1]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 5000
+-1000 <= nums[i] <= 1000
+*/
+
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        int res = 0;
+        if(n<=2) return 0;
+        for(int i=2;i<n;++i){
+            if(nums[i]-nums[i-1] == nums[i-1]-nums[i-2]) count++;
+            else count=0;
+            res+=count;
+        }
+        return res;
+    }
+}
